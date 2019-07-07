80dbbdb9ecca5622248032ce479a7ef154c9d226
chang02 <chang0022@snu.ac.kr>
2019-07-07 20:19:06 +0900

    rune page layout

---
+++ src/components/Rune/RunePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-layout fill-height>
+    <v-flex fill-height>
+      <v-layout column fill-height>
+        <v-flex>
+          <v-layout fill-height>
+            <v-btn icon v-for="perk in lolstatic.perks" v-bind:key="perk.id">
+              <v-img
+                :src="`https://ddragon.leagueoflegends.com/cdn/img/${perk.icon}`"
+                style="max-width:30px;max-height:30px;"
+              />
+            </v-btn>
+          </v-layout>
+        </v-flex>
+        <v-flex>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-flex fill-height>
+      <v-layout column fill-height>
+        <v-flex>
+          <v-layout fill-height>
+            <v-btn icon v-for="perk in lolstatic.perks" v-bind:key="perk.id">
+              <v-img
+                :src="`https://ddragon.leagueoflegends.com/cdn/img/${perk.icon}`"
+                style="max-width:30px;max-height:30px;"
+              />
+            </v-btn>
+          </v-layout>
+        </v-flex>
+        <v-flex>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class ChampionInfoCard extends Vue {
+  get lolstatic() {
+    return this.$store.state.lolstatic;
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/RuneDetail.vue
+++ src/views/RuneDetail.vue
@@ -1,10 +1,22 @@
 <template>
   <v-layout fill-height column id="rune-detail-page">
-    <div>
+    <div class="mb-3">
       <v-layout align-center px-3>
         <v-img :src="champions[championId].iconUrl" class="champion-icon mr-3" />
         <span class="title">{{champions[championId].name}}</span>
       </v-layout>
+    </div>
+    <div style="width:250px;" class="px-3">
+      <v-select
+        dark attach
+        :items="items"
+        item-text="name"
+        item-value="value"
+        v-model="pageNum"
+      />
+    </div>
+    <div class="ml-5">
+      <rune-page />
     </div>
   </v-layout>
 </template>
@@ -12,12 +24,34 @@
 <script lang="ts">
 import { mapGetters } from 'vuex';
 import { Component, Vue, Prop } from 'vue-property-decorator';
+import RunePage from '../components/Rune/RunePage.vue';
 
 @Component({
-  components: {},
+  components: {
+    'rune-page': RunePage,
+  },
 })
 export default class Home extends Vue {
   @Prop() public championId!: number;
+  public pageNum: number = 1;
+  public items: any[] = [
+    {
+      name: '추천1',
+      value: 1,
+    },
+    {
+      name: '추천2',
+      value: 2,
+    },
+    {
+      name: '사용자1',
+      value: 3,
+    },
+    {
+      name: '사용자2',
+      value: 4,
+    },
+  ];
   public mounted(): void {}
 
   get champions() {
@@ -28,6 +62,9 @@ export default class Home extends Vue {
 <style scoped>
 #rune-detail-page {
   overflow-y: auto;
+}
+#rune-title {
+  height: 50px;
 }
 .champion-icon {
   min-width: 50px;
