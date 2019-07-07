cf16965acf06cd07ba9f1f3f2eb5d0cd07210f65
chang02 <chang0022@snu.ac.kr>
2019-07-07 21:04:48 +0900

    apply changed api of match champions info

--- src/components/Match/ChampionInfoCard.vue
+++ src/components/Match/ChampionInfoCard.vue
@@ -1,34 +1,105 @@
 <template>
   <v-card dark height="100%" flat class="summoner-info-card blue-grey darken-3">
-    <v-layout fill-height column class="grey darken-3">
-      <v-flex mb-3>
-        <v-layout>
-          <v-flex xs4>최근 {{champions.totalGames}} 게임</v-flex>
-          <v-flex xs1>게임수</v-flex>
-          <v-flex xs1>승률</v-flex>
-          <v-flex xs3>K/D/A</v-flex>
-          <v-flex xs1>평균 CS</v-flex>
-          <v-flex xs2>평균 골드</v-flex>
-        </v-layout>
-      </v-flex>
-      <v-flex v-for="champion in championsArray" v-bind:key="champion.key" mb-2>
-        <v-layout>
-          <v-flex xs4>
-            <v-layout align-center>
-              <v-img :src="staticChampions[champion.key].iconUrl" class="champion-icon mr-2" />
-              <span>{{staticChampions[champion.key].name}}</span>
+    <v-tabs fixed-tabs dark height=40 slider-color="blue" v-model="tab" class="mb-1 grey darken-4">
+      <v-tab class="cursor-pointer">전체</v-tab>
+      <v-tab class="cursor-pointer">솔랭</v-tab>
+      <v-tab class="cursor-pointer">자유랭</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-layout fill-height column class="grey darken-3">
+          <v-flex mb-3>
+            <v-layout>
+              <v-flex xs4>최근 {{champions.totalGames}} 게임</v-flex>
+              <v-flex xs1>게임수</v-flex>
+              <v-flex xs1>승률</v-flex>
+              <v-flex xs3>K/D/A</v-flex>
+              <v-flex xs1>평균 CS</v-flex>
+              <v-flex xs2>평균 골드</v-flex>
             </v-layout>
           </v-flex>
-          <v-flex xs1>{{champion.wins + champion.losses}}</v-flex>
-          <v-flex xs1>{{(champion.wins * 100 / (champion.wins + champion.losses)).toFixed(2)}}%</v-flex>
-          <v-flex
-            xs3
-          >{{champion.averageKills.toFixed(1)}}/{{champion.averageDeaths.toFixed(1)}}/{{champion.averageAssists.toFixed(1)}}</v-flex>
-          <v-flex xs1>{{champion.averageCS.toFixed(2)}}</v-flex>
-          <v-flex xs2>{{champion.averageEarnedGold.toFixed(2)}}</v-flex>
+          <v-flex v-for="champion in totalArray" v-bind:key="champion.key" mb-2>
+            <v-layout v-if="champion.wins !== null">
+              <v-flex xs4>
+                <v-layout align-center>
+                  <v-img :src="staticChampions[champion.key].iconUrl" class="champion-icon mr-2" />
+                  <span>{{staticChampions[champion.key].name}}</span>
+                </v-layout>
+              </v-flex>
+              <v-flex xs1>{{champion.wins + champion.losses}}</v-flex>
+              <v-flex xs1>{{(champion.wins * 100 / (champion.wins + champion.losses)).toFixed(2)}}%</v-flex>
+              <v-flex
+                xs3
+              >{{champion.averageKills.toFixed(1)}}/{{champion.averageDeaths.toFixed(1)}}/{{champion.averageAssists.toFixed(1)}}</v-flex>
+              <v-flex xs1>{{champion.averageCS.toFixed(2)}}</v-flex>
+              <v-flex xs2>{{champion.averageEarnedGold.toFixed(2)}}</v-flex>
+            </v-layout>
+          </v-flex>
         </v-layout>
-      </v-flex>
-    </v-layout>
+      </v-tab-item>
+      <v-tab-item>
+        <v-layout fill-height column class="grey darken-3">
+          <v-flex mb-3>
+            <v-layout>
+              <v-flex xs4>최근 {{champions.totalGames}} 게임</v-flex>
+              <v-flex xs1>게임수</v-flex>
+              <v-flex xs1>승률</v-flex>
+              <v-flex xs3>K/D/A</v-flex>
+              <v-flex xs1>평균 CS</v-flex>
+              <v-flex xs2>평균 골드</v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex v-for="champion in soloArray" v-bind:key="champion.key" mb-2>
+            <v-layout v-if="champion.wins !== null">
+              <v-flex xs4>
+                <v-layout align-center>
+                  <v-img :src="staticChampions[champion.key].iconUrl" class="champion-icon mr-2" />
+                  <span>{{staticChampions[champion.key].name}}</span>
+                </v-layout>
+              </v-flex>
+              <v-flex xs1>{{champion.wins + champion.losses}}</v-flex>
+              <v-flex xs1>{{(champion.wins * 100 / (champion.wins + champion.losses)).toFixed(2)}}%</v-flex>
+              <v-flex
+                xs3
+              >{{champion.averageKills.toFixed(1)}}/{{champion.averageDeaths.toFixed(1)}}/{{champion.averageAssists.toFixed(1)}}</v-flex>
+              <v-flex xs1>{{champion.averageCS.toFixed(2)}}</v-flex>
+              <v-flex xs2>{{champion.averageEarnedGold.toFixed(2)}}</v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-tab-item>
+      <v-tab-item>
+        <v-layout fill-height column class="grey darken-3">
+          <v-flex mb-3>
+            <v-layout>
+              <v-flex xs4>최근 {{champions.totalGames}} 게임</v-flex>
+              <v-flex xs1>게임수</v-flex>
+              <v-flex xs1>승률</v-flex>
+              <v-flex xs3>K/D/A</v-flex>
+              <v-flex xs1>평균 CS</v-flex>
+              <v-flex xs2>평균 골드</v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex v-for="champion in flexArray" v-bind:key="champion.key" mb-2>
+            <v-layout v-if="champion.wins !== null">
+              <v-flex xs4>
+                <v-layout align-center>
+                  <v-img :src="staticChampions[champion.key].iconUrl" class="champion-icon mr-2" />
+                  <span>{{staticChampions[champion.key].name}}</span>
+                </v-layout>
+              </v-flex>
+              <v-flex xs1>{{champion.wins + champion.losses}}</v-flex>
+              <v-flex xs1>{{(champion.wins * 100 / (champion.wins + champion.losses)).toFixed(2)}}%</v-flex>
+              <v-flex
+                xs3
+              >{{champion.averageKills.toFixed(1)}}/{{champion.averageDeaths.toFixed(1)}}/{{champion.averageAssists.toFixed(1)}}</v-flex>
+              <v-flex xs1>{{champion.averageCS.toFixed(2)}}</v-flex>
+              <v-flex xs2>{{champion.averageEarnedGold.toFixed(2)}}</v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 
@@ -41,17 +112,71 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 })
 export default class ChampionInfoCard extends Vue {
   @Prop() public champions: any;
+  public tab: number = 0;
 
   public mounted() {}
 
-  get championsArray() {
+  get totalArray() {
     const champions = this.champions.champions;
     const championsArray = Object.values(champions);
-    championsArray.sort((a: any, b: any) => {
-      return b.wins + b.losses - (a.wins + a.losses);
+    const championsTotalArray: any[] = [];
+    championsArray.forEach((element: any) => {
+      if (Object.entries(element.total).length !== 0) {
+        championsTotalArray.push(element.total);
+      }
     });
-    return championsArray;
+    championsTotalArray.sort((a: any, b: any) => {
+      if (a.wins + a.losses - b.wins - b.losses < 0) {
+        return 1;
+      }
+      if (a.wins + a.losses - b.wins - b.losses > 0) {
+        return -1;
+      }
+      return 0;
+    });
+    return championsTotalArray;
   }
+  get soloArray() {
+    const champions = this.champions.champions;
+    const championsArray = Object.values(champions);
+    const championsSoloArray: any[] = [];
+    championsArray.forEach((element: any) => {
+      if (Object.entries(element.solo).length !== 0) {
+        championsSoloArray.push(element.solo);
+      }
+    });
+    championsSoloArray.sort((a: any, b: any) => {
+      if (a.wins + a.losses - b.wins - b.losses < 0) {
+        return 1;
+      }
+      if (a.wins + a.losses - b.wins - b.losses > 0) {
+        return -1;
+      }
+      return 0;
+    });
+    return championsSoloArray;
+  }
+  get flexArray() {
+    const champions = this.champions.champions;
+    const championsArray = Object.values(champions);
+    const championsFlexArray: any[] = [];
+    championsArray.forEach((element: any) => {
+      if (Object.entries(element.flex).length !== 0) {
+        championsFlexArray.push(element.flex);
+      }
+    });
+    championsFlexArray.sort((a: any, b: any) => {
+      if (a.wins + a.losses - b.wins - b.losses < 0) {
+        return 1;
+      }
+      if (a.wins + a.losses - b.wins - b.losses > 0) {
+        return -1;
+      }
+      return 0;
+    });
+    return championsFlexArray;
+  }
+
   get staticChampions() {
     return this.$store.state.lolstatic.champions;
   }
