434d28a75c400feccf4ba273ebf5f3695cc72fda
Lee Chang Young <31840656+chang02@users.noreply.github.com>
2019-07-22 23:47:13 +0900

    can save perk for champions

--- src/components/Rune/RunePage.vue
+++ src/components/Rune/RunePage.vue
@@ -6,12 +6,12 @@
           <v-layout fill-height>
             <v-btn
               icon v-for="perkId in perkIdList" v-bind:key="perkId"
-              @click="perk.primary.id=perkId"
-              :color="perkId===perk.primary.id?'orange':''" :outline="perkId===perk.primary.id"
+              @click="copyPerk.primary.id=perkId"
+              :color="perkId===copyPerk.primary.id?'orange':''" :outline="perkId===copyPerk.primary.id"
             >
               <v-img
                 :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[perkId].icon}`"
-                class="perk-icon" :class="perkId===perk.primary.id?'selected':''"
+                class="perk-icon" :class="perkId===copyPerk.primary.id?'selected':''"
               />
             </v-btn>
           </v-layout>
@@ -19,13 +19,13 @@
         <div v-for="line in [0, 1, 2, 3]" v-bind:key="line">
           <v-layout fill-height>
             <v-btn
-              icon v-for="perkId in Object.keys(lolstatic.perks[perk.primary.id].slots[line].runes)" v-bind:key="perkId"
-              @click="perk.primary[`slot${line}`] = perkId"
-              :color="perk.primary[`slot${line}`]===perkId?'orange':''" :outline="perk.primary[`slot${line}`]===perkId"
+              icon v-for="perkId in Object.keys(lolstatic.perks[copyPerk.primary.id].slots[line].runes)" v-bind:key="perkId"
+              @click="copyPerk.primary[`slot${line}`] = perkId"
+              :color="copyPerk.primary[`slot${line}`]===perkId?'orange':''" :outline="copyPerk.primary[`slot${line}`]===perkId"
             >
             <v-img
-              :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[perk.primary.id].slots[line].runes[perkId].icon}`"
-              class="perk-icon" :class="perk.primary[`slot${line}`]===perkId?'selected':''"
+              :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[copyPerk.primary.id].slots[line].runes[perkId].icon}`"
+              class="perk-icon" :class="copyPerk.primary[`slot${line}`]===perkId?'selected':''"
             />
             </v-btn>
           </v-layout>
@@ -38,13 +38,13 @@
           <v-layout fill-height>
             <v-btn
               icon v-for="perkId in perkIdList" v-bind:key="perkId"
-              v-show="perkId !== perk.primary.id"
-              @click="perk.secondary.id=perkId"
-              :color="perkId===perk.secondary.id?'orange':''" :outline="perkId===perk.secondary.id"
+              v-show="perkId !== copyPerk.primary.id"
+              @click="copyPerk.secondary.id=perkId"
+              :color="perkId===copyPerk.secondary.id?'orange':''" :outline="perkId===copyPerk.secondary.id"
             >
               <v-img
                 :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[perkId].icon}`"
-                class="perk-icon" :class="perkId===perk.secondary.id?'selected':''"
+                class="perk-icon" :class="perkId===copyPerk.secondary.id?'selected':''"
               />
             </v-btn>
           </v-layout>
@@ -52,94 +52,86 @@
         <div v-for="line in [1, 2, 3]" v-bind:key="line">
           <v-layout fill-height>
             <v-btn
-              icon v-for="perkId in Object.keys(lolstatic.perks[perk.secondary.id].slots[line].runes)" v-bind:key="perkId"
-              @click="perk.secondary[`slot${line}`] = perkId"
-              :color="perk.secondary[`slot${line}`]===perkId?'orange':''" :outline="perk.secondary[`slot${line}`]===perkId"
+              icon v-for="perkId in Object.keys(lolstatic.perks[copyPerk.secondary.id].slots[line].runes)" v-bind:key="perkId"
+              @click="copyPerk.secondary[`slot${line}`] = perkId"
+              :color="copyPerk.secondary[`slot${line}`]===perkId?'orange':''" :outline="copyPerk.secondary[`slot${line}`]===perkId"
             >
             <v-img
-              :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[perk.secondary.id].slots[line].runes[perkId].icon}`"
-              class="perk-icon" :class="perk.secondary[`slot${line}`]===perkId?'selected':''"
+              :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[copyPerk.secondary.id].slots[line].runes[perkId].icon}`"
+              class="perk-icon" :class="copyPerk.secondary[`slot${line}`]===perkId?'selected':''"
             />
             </v-btn>
           </v-layout>
         </div>
+        <div>
           <v-layout fill-height>
             <v-btn
-              icon v-for="perkId in [5008, 5005, 5007]" v-bind:Key="perkId"
-              @click="perk.stat.stat1=perkId"
-              :color="perk.stat.stat1===perkId?'orange':''" :outline="perk.stat.stat1===perkId"
+              icon v-for="perkId in ['5008', '5005', '5007']" v-bind:Key="perkId"
+              @click="copyPerk.stat.stat1=perkId"
+              :color="copyPerk.stat.stat1===perkId?'orange':''" :outline="copyPerk.stat.stat1===perkId"
             >
               <v-img
-                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[5000].slots[0].runes[perkId].icon}`"
-                class="perk-icon" :class="perk.stat.stat1===perkId?'selected':''"
+                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks['5000'].slots[0].runes[perkId].icon}`"
+                class="perk-icon" :class="copyPerk.stat.stat1===perkId?'selected':''"
               />
             </v-btn>
           </v-layout>
-        <div>
-          </div>
-          <v-layout fill-height>
-            <v-btn
-              icon v-for="perkId in [5008, 5002, 5003]" v-bind:Key="perkId"
-              @click="perk.stat.stat2=perkId"
-              :color="perk.stat.stat2===perkId?'orange':''" :outline="perk.stat.stat2===perkId"
-            >
-              <v-img
-                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[5000].slots[0].runes[perkId].icon}`"
-                class="perk-icon" :class="perk.stat.stat2===perkId?'selected':''"
-              />
-            </v-btn>
-          </v-layout>
-        <div>
-          </div>
-          <v-layout fill-height>
-            <v-btn
-              icon v-for="perkId in [5001, 5002, 5003]" v-bind:Key="perkId"
-              @click="perk.stat.stat3=perkId"
-              :color="perk.stat.stat3===perkId?'orange':''" :outline="perk.stat.stat3===perkId"
-            >
-              <v-img
-                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks[5000].slots[0].runes[perkId].icon}`"
-                class="perk-icon" :class="perk.stat.stat3===perkId?'selected':''"
-              />
-            </v-btn>
-          </v-layout>
-        <div>
         </div>
+        <div>
+          <v-layout fill-height>
+            <v-btn
+              icon v-for="perkId in ['5008', '5002', '5003']" v-bind:Key="perkId"
+              @click="copyPerk.stat.stat2=perkId"
+              :color="copyPerk.stat.stat2===perkId?'orange':''" :outline="copyPerk.stat.stat2===perkId"
+            >
+              <v-img
+                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks['5000'].slots[0].runes[perkId].icon}`"
+                class="perk-icon" :class="copyPerk.stat.stat2===perkId?'selected':''"
+              />
+            </v-btn>
+          </v-layout>
+        </div>
+        <div>
+          <v-layout fill-height>
+            <v-btn
+              icon v-for="perkId in ['5001', '5002', '5003']" v-bind:Key="perkId"
+              @click="copyPerk.stat.stat3=perkId"
+              :color="copyPerk.stat.stat3===perkId?'orange':''" :outline="copyPerk.stat.stat3===perkId"
+            >
+              <v-img
+                :src="`${lolstatic.perks.baseIconUrl}${lolstatic.perks['5000'].slots[0].runes[perkId].icon}`"
+                class="perk-icon" :class="copyPerk.stat.stat3===perkId?'selected':''"
+              />
+            </v-btn>
+          </v-layout>
+        </div>
+        <v-btn @click="save">save</v-btn>
       </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component({
   components: {},
 })
 export default class ChampionInfoCard extends Vue {
-  public perkIdList: any[] = [8000, 8100, 8200, 8300, 8400];
-  public perk: any = {
-    primary: {
-      id: 8000,
-      slot0: 0,
-      slot1: 0,
-      slot2: 0,
-      slot3: 0,
-    },
-    secondary: {
-      id: 8100,
-      slot1: 0,
-      slot2: 0,
-      slot3: 0,
-    },
-    stat: {
-      stat1: 0,
-      stat2: 0,
-      stat3: 0,
-    },
-  };
+  @Prop() public perk!: any;
+  public copyPerk: any = this.perk;
+  public perkIdList: any[] = ['8000', '8100', '8200', '8300', '8400'];
+
+  @Watch('perk')
+  public onPerkChange(to: any, from: any) {
+    this.copyPerk = to;
+  }
+
   get lolstatic() {
     return this.$store.state.lolstatic;
+  }
+  public save() {
+    this.$emit('save', this.copyPerk);
   }
 }
 </script>
