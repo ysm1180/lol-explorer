27bffbe1a7b99ea2945d72ee30551c79bf3a5b36
Seongmin Yeon <ysm1180@gmail.com>
2019-07-24 20:02:17 +0900

    Modify design in match detail

---
+++ src/typings/summoner.d.ts
@@ -0,0 +1,30 @@
+export interface ISummonerApiData {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+  updatedTs: string;
+  seasons: ISummonerSeason[];
+  iconUrl: string;
+}
+
+export interface ISummonerSeason {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: string;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+  season: number;
+  miniSeries?: string[];
+}
