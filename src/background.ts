d9f5df31b14214ad9146f09cb4a3fc89eb51a13e
Seongmin Yeon <ysm1180@gmail.com>
2019-08-16 02:41:14 +0900

    Production 모드에서 storage 접근 거부 문제 픽스

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-import { app, ipcMain } from 'electron';
+import { app, ipcMain, protocol } from 'electron';
 import LCUConnector from 'lcu-connector';
 import { AppWindow } from './electron/window';
 
@@ -14,6 +14,9 @@ const gotSingleInstanceLock = app.requestSingleInstanceLock();
 isDuplicateInstance = !gotSingleInstanceLock;
 
 app.commandLine.appendSwitch('--ignore-certificate-errors');
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } },
+]);
 
 app.on('second-instance', (event, args, workingDirectory) => {
   // Someone tried to run a second instance, we should focus our window.
